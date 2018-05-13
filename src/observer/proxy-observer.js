023767d690623575daf5706bbc1af52187be5dda
nossika <476492581@qq.com>
2018-05-13 13:45:47 +0800

    fix falsish error

--- src/observer/proxy-observer.js
+++ src/observer/proxy-observer.js
@@ -3,11 +3,12 @@ import Dep from '../dependence';
 function proxyObserver (obj) {
     const cacheMap = Object.create(null);
     return new Proxy(obj, {
-        has (key) {
-            return true;
+        has (target, key) {
+            return key in target;
         },
         get (target, key) {
             // return target[key];
+
             if (key in cacheMap) {
                 Dep.target && cacheMap[key].dep.addSub(Dep.target);
                 return cacheMap[key].value;
@@ -29,10 +30,8 @@ function proxyObserver (obj) {
             return value;
         },
         set (target, key, rawValue) {
-            // target[key] = rawValue;
-            // return true;
-            if (target[key] === rawValue) return;
-            target[key] = rawValue;
+            if (target[key] === rawValue) return true;
+            Reflect.set(target, key, rawValue);
             let value;
             if (rawValue && typeof rawValue === 'object') {
                 value = proxyObserver(target[key]);
